0c84be3a7128bb088c10abb35e32c524c60023e2
Aman Rawat <amannraawat@gmail.com>
2024-04-11 23:52:42 +0530

    done little changes on the designing

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '023772ef3f65adc59f6ef24456d09d4d0fec1b86f52a10712e4d8459737107e7'
 
 @app.route('/')
 def home():
@@ -9,6 +12,23 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/store')
+def store():
+    return render_template('store.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}", "success")
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
