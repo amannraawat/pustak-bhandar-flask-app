b54ecd814e0736c6409a122d801fca7d5c4c9113
Aman Rawat <amannraawat@gmail.com>
2024-05-10 17:01:58 +0530

    update code and now will be adding new functionality to application

--- pustak_bhandar/__init__.py
+++ pustak_bhandar/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '023772ef3f65adc59f6ef24456d09d4d0fec1b86f52a10712e4d8459737107e7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, session_options={"autoflush": False})
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
